7f0540988194e3a73d2f77f4b5eee03362048cd7
Kev Mcgrady <kevinmcgrady47@gmail.com>
2024-02-23 10:09:37 +0000

    feat/video-header

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -5,11 +5,23 @@ type HeroProps = {
 
 export const Hero = ({ title, subtitle }: HeroProps) => {
   return (
-    <div className='h-[400px] bg-gradient-to-tl from-[#00c6ff] to-[#0072ff] mb-5 rounded-xl flex items-center justify-center flex-col'>
-      <h1 className='font-bold text-9xl text-white text-center font-shadow mb-4'>
-        {title}
-      </h1>
-      <h2 className='font-light text-xl text-center'>{subtitle}</h2>
-    </div>
+    <header className='relative flex items-center justify-center h-[400px] mb-5 overflow-hidden rounded-xl'>
+      <div className='relative z-30 p-5 text-2xl rounded-xl bg-gradient-to-tl from-[#00c6ff] to-[#0072ff] opacity-50 h-full w-full flex items-center justify-center flex-col' />
+      <div className='absolute z-40'>
+        <h1 className='font-bold text-9xl text-white text-center font-shadow mb-4'>
+          {title}
+        </h1>
+        <h2 className='font-light text-xl text-center'>{subtitle}</h2>
+      </div>
+      <video
+        autoPlay
+        muted
+        loop
+        className='absolute z-10 w-auto min-w-full min-h-full max-w-none'
+      >
+        <source src='/videos/hero-video.mp4' type='video/mp4' />
+        Your browser does not support the video tag.
+      </video>
+    </header>
   );
 };
